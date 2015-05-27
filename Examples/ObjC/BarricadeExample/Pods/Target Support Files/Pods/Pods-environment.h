74f7144d4473451eebaecd814910a7184040e0e0
John McIntosh <johntmcintosh@gmail.com>
2015-05-27 11:09:46 -0500

    Adjust default pod subspec

--- Examples/ObjC/BarricadeExample/Pods/Target Support Files/Pods/Pods-environment.h
+++ Examples/ObjC/BarricadeExample/Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// MMBarricade
+#define COCOAPODS_POD_AVAILABLE_MMBarricade
+#define COCOAPODS_VERSION_MAJOR_MMBarricade 1
+#define COCOAPODS_VERSION_MINOR_MMBarricade 0
+#define COCOAPODS_VERSION_PATCH_MMBarricade 0
+
 // MMBarricade/Core
 #define COCOAPODS_POD_AVAILABLE_MMBarricade_Core
 #define COCOAPODS_VERSION_MAJOR_MMBarricade_Core 1
